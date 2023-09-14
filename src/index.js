97ee56283a19060a950079d64f9e3b9c8489fce5
martynenkovlad <108723639+martynenkovlad@users.noreply.github.com>
2023-09-14 17:00:37 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function reverseArray(arr) {
+    return arr.reverse();
+  }
+  
+  const originalArray = [1, 2, 3, 4, 5];
+  const reversedArray = reverseArray(originalArray);
+  console.log(reversedArray);
